6dddcd327c3e74c8020fe32761575696a7bda1be
Wisnu Prasodjo <wisnucakraa@gmail.com>
2021-11-24 20:55:50 +0700

    init

--- bin/first_learn_dart.dart
+++ bin/first_learn_dart.dart
@@ -1,3 +1,8 @@
 import 'dart:io';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  String input = stdin.readLineSync();
+  double number = double.tryParse(input);
+  // print(input);
+  print(number * 2);
+}
