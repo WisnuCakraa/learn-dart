050bfd407fea16e00aa8c09594dd4cd9a4bfc77f
Wisnu Prasodjo <helen.72@gmail.com>
2021-12-01 10:21:12 +0700

    init mixin

---
+++ bin/oop/mixin/monster.dart
@@ -0,0 +1,6 @@
+import 'character.dart';
+
+abstract class Monster extends Character {
+  String eatHero() => "hmmm yummy... grr";
+  String move();
+}
---
+++ bin/oop/mixin/monster_ucoa.dart
@@ -0,0 +1,9 @@
+import 'monster_ubur.dart';
+import 'flying_monster.dart';
+
+class MonsterUcoa extends MonsterUbur implements FlyingMonster {
+  @override
+  String fly() {
+    return "fly!";
+  }
+}
